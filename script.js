fa3d0138aac900a4909f94965d054c7321f5054c
Inés Sanz <inessanzm@hotmail.es>
2024-12-17 16:13:38 +0000

    feat: menu translate

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const homeCarousel = document.querySelector('#myCarousel')
+const homeCarousel = document.querySelector("#myCarousel");
+const traducir = document.querySelector("#traducir");
+const menu = document.querySelector("#menu");
+const menuingles = document.querySelector("#menuIngles");
 
 const carousel = new bootstrap.Carousel(homeCarousel, {
   interval: 2000,
-  touch: false
-})
+  touch: false,
+});
+
+const changemenu = () => {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    menuingles.classList.add("active");
+  } else {
+    menuingles.classList.remove("active");
+    menu.classList.add("active");
+  }
+  console.log("changemenu")
+};
+
+traducir.addEventListener('click', changemenu);
