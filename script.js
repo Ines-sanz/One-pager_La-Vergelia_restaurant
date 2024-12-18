cc78bcff4198bc87b9b76d22eb55b3fabde10297
Inés Sanz <inessanzm@hotmail.es>
2024-12-18 13:47:57 +0000

    feat: animations added

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const homeCarousel = document.querySelector("#myCarousel");
 const traducir = document.querySelector("#traducir");
 const menu = document.querySelector("#menu");
 const menuingles = document.querySelector("#menuIngles");
+const icon = document.querySelector(".my-icons");
+const naranja = document.querySelector(".naranja")
+const frutas = document.querySelector(".frutas")
+
 
 const carousel = new bootstrap.Carousel(homeCarousel, {
   interval: 2000,
@@ -20,3 +24,23 @@ const changemenu = () => {
 };
 
 traducir.addEventListener('click', changemenu);
+
+window.addEventListener("scroll", function () {
+  const position = icon.getBoundingClientRect();
+  if (position.top < window.innerHeight && position.bottom > 0) {
+      icon.classList.add("show");
+  }
+});
+
+window.addEventListener("scroll", function () {
+  const position = naranja.getBoundingClientRect();
+  if (position.top < window.innerHeight && position.bottom > 0) {
+      naranja.classList.add("show");
+  }
+});
+window.addEventListener("scroll", function () {
+  const position = frutas.getBoundingClientRect();
+  if (position.top < window.innerHeight && position.bottom > 0) {
+      frutas.classList.add("show");
+  }
+});
